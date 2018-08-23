c008498218cecbdae805cfaf8adda8cd2856f4c7
Rey <Rey>
2018-08-23 09:45:11 +0800

    [脚本]更新安装 jdk 考本

---
+++ sh/download_jdk8.sh
@@ -0,0 +1,5 @@
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz ~/
+cd
+tar -zxvf jdk-8* jdk8
+cd jdk8
+pwd
