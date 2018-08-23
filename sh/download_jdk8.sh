84b0eac4d508f9cbfc41c09ab80082f04342d9d9
Rey <Rey>
2018-08-23 19:45:48 +0800

    [sh] update download_jdk.sh

--- sh/download_jdk8.sh
+++ sh/download_jdk8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 从 oracle 官网下载jdk，需要同意下载协议，直接用 wget下载，默认是不同意的，虽然可以下，但是是有问题的
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz" -P ~/jdk8
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz" -O ~/jdk8.tar.gz
 cd
-tar -zxvf jdk8
+tar -zxvf jdk8.tar.gz
 cd jdk8
 pwd
