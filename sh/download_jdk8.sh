99cff0da81e42e6a4432964e54b30ae214dd57ad
Rey <Rey>
2018-09-27 20:00:42 +0800

    哦

--- sh/download_jdk8.sh
+++ sh/download_jdk8.sh
@@ -3,6 +3,14 @@
 # -P 只是指定了目录前缀， -O 重命名，但是 -P 和 -O 一起使用 -O就失效了
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz" -O ~/jdk8.tar.gz
 cd
-tar -zxvf jdk8.tar.gz
+tar -zxvf jdk8.tar.gz jdk8
 cd jdk8
 pwd
+
+# JDK 配置 PATH
+echo "JAVA_HOME=$(pwd)" >> /etc/profile
+echo "CLASSPATH=$JAVA_HOME/lib/" >> /etc/profile
+echo "PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile
+echo "export PATH JAVA_HOME CLASSPATH" >> /etc/profile
+source /etc/profile
+java -version
